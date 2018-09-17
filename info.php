1336256c0e196d68db32d0b8e5b5ea436fa708b3
renupetori <renupetori@gmail.com>
2018-09-17 13:21:51 +0300

    muutosta

--- info.php
+++ info.php
@@ -25,6 +25,22 @@ include 'shared/nav.php';
 	</aside>
 	<article>
 			<h1>Taulukko</h1>
+			<table>
+				<tr>
+					<td>1</td>
+					<td>2</td>
+					<td>3</td>
+				</tr>
+				<tr>
+					<td>4</td>
+					<td>5</td>
+					<td>6</td>
+				</tr>
+				<tr>
+					<td>7</td>
+					<td>8</td>
+					<td>9</td>
+				</tr>
 			
 	</article>
 	
