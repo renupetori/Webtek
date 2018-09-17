9ed92e3d13f78218fc03c1f74edbe3befb1f4bc8
renupetori <renupetori@gmail.com>
2018-09-17 15:31:32 +0300

    js

--- shared/nav.php
+++ shared/nav.php
@@ -3,4 +3,12 @@
 	<a href="posts.php">Posts</a>
 	<a href="info.php">Info</a>
 	<a href="contact.php">Contact</a>
+	
+	<a href="#" id="js" onclick="vip">JavaScript</a>
+	
+	<script>
+function vip() {
+    alert("Coming Soon");
+}
+</script>
 </nav>
